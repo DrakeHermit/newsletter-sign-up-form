86390a753816b86ce7165676fef165cd53821935
Jan Kotvaš <jan.kotvas96@gmail.com>
2024-06-01 22:46:15 +0200

    Added the on success message card

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const checkMarkSvg = `<svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" viewBox="0 0 64 64"><defs><linearGradient id="a" x1="100%" x2="0%" y1="0%" y2="100%"><stop offset="0%" stop-color="#FF6A3A"/><stop offset="100%" stop-color="#FF527B"/></linearGradient></defs><g fill="none"><circle cx="32" cy="32" r="32" fill="url(#a)"/><path stroke="#FFF" stroke-width="4" d="m18.286 34.686 8.334 7.98 19.094-18.285"/></g></svg>`;
+
 // Change the illustration based on screensize
 const screenWidth = window.innerWidth;
 const illustration = document.getElementById("illustration");
@@ -36,20 +38,20 @@ const removeErrorState = () => {
 };
 
 const openOnSuccess = () => {
-  const div = createEl("div");
-  div.classList.add("main__card");
-  div.innerHTML = `
-    <h1>Hello ${emailInput.value}</h1>
+  mainContent.innerHTML = `
+    ${checkMarkSvg}
+    <h1>Thanks for subscribing!</h1>
+    <p>A confirmation email has been sent to <span>${emailInput.value}.</span> Please open it and click the button inside to confirm your subscription.</p>
+
   `;
 
-  body.appendChild(div);
-  return div;
+  mainContent.classList.remove("main__card");
+  mainContent.classList.add("main__card-success");
 };
 
 const emailInput = document.getElementById("email");
 const form = document.getElementById("form");
 const mainContent = document.getElementById("main__card");
-const body = document.getElementById("main");
 
 const handleSubmit = (e) => {
   e.preventDefault();
@@ -61,7 +63,6 @@ const handleSubmit = (e) => {
     createEl();
     removeErrorState();
   } else {
-    mainContent.remove();
     openOnSuccess();
   }
 
